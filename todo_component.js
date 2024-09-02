b044530c75a47fb64adb654452c974c38d30dac5
SirCartier50 <146313078+SirCartier50@users.noreply.github.com>
2024-09-02 03:40:11 -0700

    Add files via upload

---
+++ todo_component.js
@@ -0,0 +1,44 @@
+import { Container, TextField, Button, Box, Stack, Typography, FormGroup, FormControlLabel, Checkbox } from "@mui/material";
+import { useState } from "react";
+
+
+function Todo(){
+
+    const[item, setItem] = useState('')
+    const[list, setList] = useState([])
+
+    const addItemToList = () => {
+        if (item != []) {
+            setList([...list, item]);
+            setItem('');
+        }
+    };
+
+    return(
+            <Box 
+                width="100vw" 
+                height="100vh" 
+                display = "flex" 
+                alignItems="center"
+                flexDirection = "column"
+                gap={2}
+                mt={8}
+            >
+                <Typography variant="h3">Todo List</Typography>
+                <Stack direction="row" spacing={2}>
+                    <TextField value={item} label="Add Item" onChange={(e)=>setItem(e.target.value)}/>
+                    <Button variant="contained" onClick={addItemToList}>Add</Button>
+                </Stack>
+                
+                <Stack direction="column">
+                    {list.map((entry)=> (
+                        <FormGroup>
+                            <FormControlLabel control={<Checkbox />} label= {entry} />
+                        </FormGroup>
+                    ))}
+                </Stack>
+            </Box>
+    )
+};
+
+export default Todo;
